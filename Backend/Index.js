f9ee6904084fab3b03449701c8698a6ae6b50898
harini-elango <harini17elango@gmail.com>
2023-07-21 15:50:02 +0530

    My first commit

--- Backend/Index.js
+++ Backend/Index.js
@@ -1,13 +1,19 @@
-// Express.js, or simply Express, is a back end web application framework for building RESTful APIs with Node.js
+// app.js or index.js
 import express from 'express';
 
 // import body-parser - helps to parse the request and create the req.body object
 import bodyParser from "body-parser";
 
+// import cors - provides Express middleware to enable CORS with various options, connect frontend
+import cors from "cors";
+
 // import routes
 import router from './Routes/routes.js';
 
 const app = express();
+
+// Enable CORS for all routes
+app.use(cors());
 
 // use express json
 app.use(bodyParser.json());
@@ -24,7 +30,7 @@ app.get('/', function(req, res){
 });
 
 // Start the server
-const port = 4000;
+const port = 4000; // You can choose any port you prefer
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
